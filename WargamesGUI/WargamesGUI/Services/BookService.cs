58b14ea6fec3043c3662324ba538a988ed58bd41
alexstenberg <70631861+alexstenberg@users.noreply.github.com>
2021-05-04 14:33:19 +0200

    Update
    BookService - Add/Remove/Update
    EventService - Add/Remove/Update

--- WargamesGUI/WargamesGUI/Services/BookService.cs
+++ WargamesGUI/WargamesGUI/Services/BookService.cs
@@ -4,30 +4,57 @@ using System.Text;
 using System.Linq;
 using System.Data;
 using System.Data.SqlClient;
+using WargamesGUI.Models;
+using System.Threading.Tasks;
 
 namespace WargamesGUI.Services
 {
-    class BookService
+    class BookService : DbHandler
     {
-        private const string theConString = "Server=tcp:wargameslibrary.database.windows.net,1433;Initial Catalog=Wargames Library;Persist Security Info=False;User ID=adminwargames;Password=Admin123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
-        private const string theBookTableName = "tblBook";
-        private const string theObjectsRemovedTableName = "";
-
-        private string queryForBookListPage = string.Empty;
-
-        public DataTable GetBooksFromDb()
+        
+        public async Task<List<Book>> GetBooksFromDb()
         {
+            var bookList = new List<Book>();
+
             using (SqlConnection con = new SqlConnection(theConString))
             {
                 con.Open();
-                SqlCommand com = new SqlCommand(queryForBookListPage, con);
-                SqlDataAdapter sda = new SqlDataAdapter(com);
-                DataTable dt = new DataTable();
-                sda.Fill(dt);
-                return dt;
+                using (var command = new SqlCommand(queryForBookListPage, con))
+                {
+                    using (var reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            var book = new Book();
+
+                            book.fk_Item_Id = 1;
+                            book.Title = reader["Title"].ToString();
+                            book.ISBN = reader["ISBN"].ToString();
+                            book.Publisher = reader["Publisher"].ToString();
+                            book.Description = reader["Description"].ToString();
+                            book.Price = Convert.ToInt32(reader["Price"]);
+                            book.Placement = reader["Placement"].ToString();
+                            bookList.Add(book);
+                        }
+                    }
+                }
+
+                return bookList;
             }
         }
-        public bool AddNewBook(string Title, string ISBN, string Publisher, string Description, int Price, string Placement)
+
+        /// <summary>
+        /// Adderar en ny bok till table tblBook. 
+        /// Måste skickas med: Title, ISBN, Publisher, Description, Price, Placement till boken. 
+        /// Om det är eBook eller inte - då behövs inte parametern Placement.
+        /// </summary>
+        /// <param name=""></param>
+        /// <returns>
+        /// Retunerar en bool som är true om det gick att lägga till boken eller false 
+        /// ifall det inte gick att lägga till boken.'
+        /// </returns>
+        public async Task<bool> AddNewBook(string Title, string ISBN, string Publisher,
+                                           string Description, int Price, string Placement, bool eBook)
         {
 
             bool success = true;
@@ -36,10 +63,66 @@ namespace WargamesGUI.Services
             {
                 using (SqlConnection con = new SqlConnection(theConString))
                 {
-                    string sql = 
+                    if (eBook == false)
+                    {
+                        string sql =
                         $"INSERT INTO {theBookTableName}" +
                         $"(fk_category_Id, Title, ISBN, Publisher, Description, Price, Placement) " +
                         $"VALUES('1', '{Title}', '{ISBN}', '{Publisher}', '{Description}', '{Price}', '{Placement}')";
+
+                        con.Open();
+                        using (SqlCommand cmd = new SqlCommand(sql, con))
+                        {
+                            cmd.ExecuteNonQuery();
+                        }
+                    }
+                    else if (eBook == true)
+                    {
+                        string sql =
+                        $"INSERT INTO {theBookTableName}" +
+                        $"(fk_category_Id, Title, ISBN, Publisher, Description, Price) " +
+                        $"VALUES(2', '{Title}', '{ISBN}', '{Publisher}', '{Description}', '{Price}')";
+
+                        con.Open();
+                        using (SqlCommand cmd = new SqlCommand(sql, con))
+                        {
+                            cmd.ExecuteNonQuery();
+                        }
+                    }
+                }
+
+                return success;
+            }
+
+            catch (Exception)
+            {
+                success = false;
+                return success;
+            }
+        }
+
+        /// <summary>
+        /// Tar bort en bok från table tblBook. 
+        /// Måste skickas med: Bokens unika ID.
+        /// </summary>
+        /// <param name=""></param>
+        /// <returns>
+        /// Retunerar en bool som är true om det gick att ta bort boken eller false 
+        /// ifall det inte gick att ta bort boken.'
+        /// </returns>
+        public async Task<bool> RemoveBook(int id)
+        {
+            bool success = true;
+
+            try
+            {
+                using (SqlConnection con = new SqlConnection(theConString))
+                {
+                    string sql = 
+                        $"DELETE FROM {theBookTableName} WHERE Id = {id}";
+
+                    // Här ska det finnas en till SQL-sträng som lägger till det borttagna objektet i en "ObjectsRemoved"-table.
+                    // Här ska det finnas en till SQL-sträng som tar bort objektet i alla tables där den är kopplad.
 
                     con.Open();
                     using (SqlCommand cmd = new SqlCommand(sql, con))
@@ -57,157 +140,18 @@ namespace WargamesGUI.Services
                 return success;
             }
         }
-        //public bool AddNewBook(string Title, string ISBN, string Publisher, string Description, int Price)
-        //{
 
-        //    bool success = true;
-
-        //    try
-        //    {
-        //        using (SqlConnection con = new SqlConnection(theConString))
-        //        {
-        //            string sql =
-        //                $"INSERT INTO tbl{theBookTableName}" +
-        //                $"(fk_category_Id, Title, ISBN, Publisher, Description, Price, Placement) " +
-        //                $"VALUES('1', '{Title}', '{ISBN}', '{Publisher}', '{Description}', '{Price}')";
-
-        //            con.Open();
-        //            using (SqlCommand cmd = new SqlCommand(sql, con))
-        //            {
-        //                cmd.ExecuteNonQuery();
-        //            }
-        //        }
-
-        //        return success;
-        //    }
-
-        //    catch (Exception)
-        //    {
-        //        success = false;
-        //        return success;
-        //    }
-        //}
-        //public bool AddNewBook(string Title, string ISBN, string Publisher, string Description)
-        //{
-
-        //    bool success = true;
-
-        //    try
-        //    {
-        //        using (SqlConnection con = new SqlConnection(theConString))
-        //        {
-        //            string sql =
-        //                $"INSERT INTO tbl{theBookTableName}" +
-        //                $"(fk_category_Id, Title, ISBN, Publisher, Description, Price, Placement) " +
-        //                $"VALUES('1', '{Title}', '{ISBN}', '{Publisher}', '{Description}')";
-
-        //            con.Open();
-        //            using (SqlCommand cmd = new SqlCommand(sql, con))
-        //            {
-        //                cmd.ExecuteNonQuery();
-        //            }
-        //        }
-
-        //        return success;
-        //    }
-
-        //    catch (Exception)
-        //    {
-        //        success = false;
-        //        return success;
-        //    }
-        //}
-        //public bool AddNewBook(string Title, string ISBN, string Publisher)
-        //{
-
-        //    bool success = true;
-
-        //    try
-        //    {
-        //        using (SqlConnection con = new SqlConnection(theConString))
-        //        {
-        //            string sql =
-        //                $"INSERT INTO tbl{theBookTableName}" +
-        //                $"(fk_category_Id, Title, ISBN, Publisher, Description, Price, Placement) " +
-        //                $"VALUES('1', '{Title}', '{ISBN}', '{Publisher}')";
-
-        //            con.Open();
-        //            using (SqlCommand cmd = new SqlCommand(sql, con))
-        //            {
-        //                cmd.ExecuteNonQuery();
-        //            }
-        //        }
-
-        //        return success;
-        //    }
-
-        //    catch (Exception)
-        //    {
-        //        success = false;
-        //        return success;
-        //    }
-        //}
-        //public bool AddNewBook(string Title, string ISBN)
-        //{
-
-        //    bool success = true;
-
-        //    try
-        //    {
-        //        using (SqlConnection con = new SqlConnection(theConString))
-        //        {
-        //            string sql =
-        //                $"INSERT INTO tbl{theBookTableName}" +
-        //                $"(fk_category_Id, Title, ISBN, Publisher, Description, Price, Placement) " +
-        //                $"VALUES('1', '{Title}', '{ISBN}')";
-
-        //            con.Open();
-        //            using (SqlCommand cmd = new SqlCommand(sql, con))
-        //            {
-        //                cmd.ExecuteNonQuery();
-        //            }
-        //        }
-
-        //        return success;
-        //    }
-
-        //    catch (Exception)
-        //    {
-        //        success = false;
-        //        return success;
-        //    }
-        //}
-        //public bool AddNewBook(string Title)
-        //{
-
-        //    bool success = true;
-
-        //    try
-        //    {
-        //        using (SqlConnection con = new SqlConnection(theConString))
-        //        {
-        //            string sql =
-        //                $"INSERT INTO tbl{theBookTableName}" +
-        //                $"(fk_category_Id, Title, ISBN, Publisher, Description, Price, Placement) " +
-        //                $"VALUES('1', '{Title}')";
-
-        //            con.Open();
-        //            using (SqlCommand cmd = new SqlCommand(sql, con))
-        //            {
-        //                cmd.ExecuteNonQuery();
-        //            }
-        //        }
-
-        //        return success;
-        //    }
-
-        //    catch (Exception)
-        //    {
-        //        success = false;
-        //        return success  ;
-        //    }
-        //}
-        public bool RemoveBook(int id)
+        /// <summary>
+        ///  
+        /// 
+        /// </summary>
+        /// <param name=""></param>
+        /// <returns>
+        /// 
+        /// 
+        /// </returns>
+        public async Task<bool> UpdateBook(int id, string Title, string ISBN, string Publisher, 
+                                           string Description, int Price, string Placement)
         {
             bool success = true;
 
@@ -215,8 +159,14 @@ namespace WargamesGUI.Services
             {
                 using (SqlConnection con = new SqlConnection(theConString))
                 {
-                    string sql = 
-                        $"DELETE FROM tbl{theBookTableName} WHERE Id = {id}";
+                    string sql =
+                        $"UPDATE {theBookTableName} " +
+                        $"SET Title = {Title}, ISBN = {ISBN}," +
+                        $"Publisher = {Publisher}, Description = {Description}" +
+                        $"Price = {Price}, Placement = {Placement}" +
+                        $"WHERE Id = {id}";
+
+                    // Här ska det finnas en till SQL-sträng som uppdaterar objektet i alla tables där den finns.
 
                     con.Open();
                     using (SqlCommand cmd = new SqlCommand(sql, con))
@@ -224,8 +174,6 @@ namespace WargamesGUI.Services
                         cmd.ExecuteNonQuery();
                     }
 
-                    // Här ska det finnas en till SQL-sträng som lägger till det borttagna objektet i en "ObjectsRemoved"-table.
-                    // Här ska det finnas en till SQL-sträng som tar bort objektet i alla tables där den är kopplad.
                 }
 
                 return success;
--- WargamesGUI/WargamesGUI/Services/EventService.cs
+++ WargamesGUI/WargamesGUI/Services/EventService.cs
@@ -1,10 +1,154 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
+using System.Data;
+using System.Data.SqlClient;
+using WargamesGUI.Models;
+using System.Threading.Tasks;
 
 namespace WargamesGUI.Services
 {
-    class EventService
+    class EventService : DbHandler
     {
+        public async Task<List<Event>> GetEventsFromDb()
+        {
+            var eventList = new List<Event>();
+
+            using (SqlConnection con = new SqlConnection(theConString))
+            {
+                con.Open();
+                using (var commad = new SqlCommand(queryForEventListPage, con))
+                {
+                    using (var reader = commad.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            var eventObject = new Event();
+
+                            eventObject.fk_Item_Id = 3;
+                            eventObject.Title = reader["Title"].ToString();
+                            eventObject.Description = reader["Description"].ToString();
+                            eventObject.DateOfEvent = Convert.ToDateTime(reader["DateOfEvent"]);
+                            eventList.Add(eventObject);
+                        }
+                    }
+                }
+
+                return eventList;
+            }
+        }
+
+        /// <summary>
+        /// Adderar ett nytt event/seminarium till table tblEvent. 
+        /// Måste skickas med: Title, Description, DateOfEvent till eventet.
+        /// </summary>
+        /// <param name=""></param>
+        /// <returns>
+        /// Retunerar en bool som är true om det gick att lägga till eventet eller false 
+        /// ifall det inte gick att lägga till eventet.'
+        /// </returns>
+        public async Task<bool> AddNewEvent(string Title, string Description, DateTime DateOfEvent)
+        {
+
+            bool success = true;
+
+            try
+            {
+                using (SqlConnection con = new SqlConnection(theConString))
+                {
+                    string sql =
+                        $"INSERT INTO {theEventTableName}" +
+                        $"(fk_category_Id, Title, Description, DateOfEvent) " +
+                        $"VALUES('3', '{Title}', '{Description}', '{DateOfEvent}')";
+
+                    con.Open();
+                    using (SqlCommand cmd = new SqlCommand(sql, con))
+                    {
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+
+                return success;
+            }
+
+            catch (Exception)
+            {
+                success = false;
+                return success;
+            }
+        }
+
+        /// <summary>
+        /// Tar bort ett event från table tblEvent. 
+        /// Måste skickas med: Eventets unika ID.
+        /// </summary>
+        /// <param name=""></param>
+        /// <returns>
+        /// Retunerar en bool som är true om det gick att ta bort eventet eller false 
+        /// ifall det inte gick att ta bort eventet.'
+        /// </returns>
+        public async Task<bool> RemoveEvent(int id)
+        {
+            bool success = true;
+
+            try
+            {
+                using (SqlConnection con = new SqlConnection(theConString))
+                {
+                    string sql =
+                        $"DELETE FROM {theEventTableName} WHERE Id = {id}";
+
+                    // Här ska det finnas en till SQL-sträng som lägger till det borttagna objektet i en "ObjectsRemoved"-table.
+                    // Här ska det finnas en till SQL-sträng som tar bort objektet i alla tables där den är kopplad.
+
+                    con.Open();
+                    using (SqlCommand cmd = new SqlCommand(sql, con))
+                    {
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+
+                return success;
+            }
+
+            catch (Exception)
+            {
+                success = false;
+                return success;
+            }
+        }
+        public bool UpdateEvent(int id, string Title, string Description, DateTime DateOfEvent)
+        {
+            bool success = true;
+
+            try
+            {
+                using (SqlConnection con = new SqlConnection(theConString))
+                {
+                    string sql =
+                        $"UPDATE {theEventTableName} " +
+                        $"SET Title = {Title}, Description = {Description}, DateOfEvent = {DateOfEvent}" +
+                        $"WHERE Id = {id}";
+
+                    // Här ska det finnas en till SQL-sträng som uppdaterar objektet i alla tables där den finns.
+
+                    con.Open();
+                    using (SqlCommand cmd = new SqlCommand(sql, con))
+                    {
+                        cmd.ExecuteNonQuery();
+                    }
+
+                }
+
+                return success;
+            }
+
+            catch (Exception)
+            {
+                success = false;
+                return success;
+            }
+        }
     }
 }
