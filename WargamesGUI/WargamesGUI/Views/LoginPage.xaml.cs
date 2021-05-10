5183e085bc4f38fce9313ce884bb6d090dd4ebbd
Niclas Edling <nke.pettersson17@hotmail.com>
2021-05-10 16:01:12 +0200

    Update
     VisitorPage

--- WargamesGUI/WargamesGUI/Views/LoginPage.xaml.cs
+++ WargamesGUI/WargamesGUI/Views/LoginPage.xaml.cs
@@ -59,8 +59,15 @@ namespace WargamesGUI
             Exception exception = null;
             try
             {
-                SearchValuePage.GetValues(SearchBar.Text);
-                App.Current.MainPage = new SearchValuePage();
+                if (String.IsNullOrWhiteSpace(SearchBar.Text))
+                {
+                    await DisplayAlert("Error", "You have to type something", "OK");
+                }
+                else
+                {
+                    SearchValuePage.GetValues(SearchBar.Text);
+                    App.Current.MainPage = new SearchValuePage();
+                }
 
             }
             catch (Exception ex)
@@ -75,50 +82,6 @@ namespace WargamesGUI
         {
             App.Current.MainPage = new SearchCardNumber();
         }
-        public class ShowHidePassEffect : PlatformEffect
-        {
-            protected override void OnAttached()
-            {
-                ConfigureControl();
-            }
 
-            protected override void OnDetached()
-            {
-            }
-
-
-            private void ConfigureControl()
-            {
-                EditText editText = ((EditText)Control);
-                editText.SetCompoundDrawablesRelativeWithIntrinsicBounds(Resource.Drawable.Password, 0, Resource.Drawable.ShowPass, 0);
-                editText.SetOnTouchListener(new OnDrawableTouchListener());
-            }
-        }
-
-        public class OnDrawableTouchListener : Java.Lang.Object, Android.Views.View.IOnTouchListener
-        {
-            public bool OnTouch(Android.Views.View v, MotionEvent e)
-            {
-                if (v is EditText && e.Action == MotionEventActions.Up)
-                {
-                    EditText editText = (EditText)v;
-                    if (e.RawX >= (editText.Right - editText.GetCompoundDrawables()[2].Bounds.Width()))
-                    {
-                        if (editText.TransformationMethod == null)
-                        {
-                            editText.TransformationMethod = PasswordTransformationMethod.Instance;
-                            editText.SetCompoundDrawablesRelativeWithIntrinsicBounds(Resource.Drawable.Password, 0, Resource.Drawable.ShowPass, 0);
-                        }
-                        else
-                        {
-                            editText.TransformationMethod = null;
-                            editText.SetCompoundDrawablesRelativeWithIntrinsicBounds(Resource.Drawable.Password, 0, Resource.Drawable.HidePass, 0);
-                        }
-                        return true;
-                    }
-                }
-                return false;
-            }
-        }
     }
 }
