7073e3b8e63c947d846145c3a65e4cf14413dd8f
alexstenberg <70631861+alexstenberg@users.noreply.github.com>
2021-05-11 14:12:01 +0200

    Update
    AddObject & BookService - Fix!
    
    Listan uppdateras. Går att lägga till samt ta bort objekt utan problem. Behövs mer felhantering.

--- WargamesGUI/WargamesGUI/Models/Book.cs
+++ WargamesGUI/WargamesGUI/Models/Book.cs
@@ -14,7 +14,7 @@ namespace WargamesGUI.Models
         public string Publisher { get; set; }
         public string Category { get; set; }
         public string Description { get; set; }
-        public int Price { get; set; }
+        public int? Price { get; set; }
         public string Placement { get; set; }
         public string Author { get; set; }
 
