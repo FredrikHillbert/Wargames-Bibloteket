0430716b6dc91213d39d45f522d7c068cdad9c7d
Niclas Edling <nke.pettersson17@hotmail.com>
2021-05-14 15:03:37 +0200

    Update Sökfunktion
    Uppdaterade listan nu man nu ser om boken finns inne eller inte för uthyrning

--- WargamesGUI/WargamesGUI/Services/BookService.cs
+++ WargamesGUI/WargamesGUI/Services/BookService.cs
@@ -306,9 +306,18 @@ namespace WargamesGUI.Services
                             values.Title = reader["Title"].ToString();
                             values.Publisher = reader["Publisher"].ToString();
                             values.fk_Item_Id = Convert.ToInt32(reader["fk_Item_Id"]);
-                            values.Price = Convert.ToInt32(reader["Price"]);
                             values.Placement = reader["Placement"].ToString();
                             values.Author = reader["Author"].ToString();
+                            values.InStock = Convert.ToInt32(reader["InStock"]);
+
+                            if (values.InStock == 0)
+                            {
+                                values.Status = "Unavailable";
+                            }
+                            else
+                            {
+                                values.Status = "Available";
+                            }
                             
                             switch (values.fk_Item_Id)
                             {
