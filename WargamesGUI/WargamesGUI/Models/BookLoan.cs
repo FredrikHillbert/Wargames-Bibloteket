bcc7de398748a3cdfedd8e3981b463e475e876c9
ro-ed <robinedin94@gmail.com>
2021-05-17 14:44:48 +0200

    Update

--- WargamesGUI/WargamesGUI/Models/BookLoan.cs
+++ WargamesGUI/WargamesGUI/Models/BookLoan.cs
@@ -6,6 +6,9 @@ namespace WargamesGUI.Models
 {
     public class BookLoan
     {
+        public int Loan_Id { get; set; }
+        public int fk_Book_Id { get; set; }
+        public int fk_LibraryCard_Id { get; set; }
         public int fk_BookLoanStatus_Id { get; set; }
         public DateTime ReturnDate { get; set; }
         public DateTime ReturnedDate { get; set; }
