ea885ad5e979acd54d4a4cbe34369d09f5d2d80e
alexstenberg <70631861+alexstenberg@users.noreply.github.com>
2021-05-20 09:03:57 +0200

    Update
    Librarycard - LoanService

---
+++ WargamesGUI/WargamesGUI/Models/LibraryCard.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WargamesGUI.Models
+{
+    public class LibraryCard
+    {
+        public int LibraryCard_Id { get; set; }
+        public string CardNumber { get; set; }
+        public int fk_Status_Id { get; set; }
+        public string Status_Level { get; set; }
+    }
+}
