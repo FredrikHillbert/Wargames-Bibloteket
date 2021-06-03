38ab1982b3023404b88bed94f0ed55047eb69576
alexstenberg <70631861+alexstenberg@users.noreply.github.com>
2021-06-03 10:04:24 +0200

    Autocomplete i LoginPage
    Fixat AutoComplete till LoginPage.
    Flytta så den finns på alla våra SearchBars.
    Behöver ev fixa någon bugg som finns, men pushar snart.

--- WargamesGUI/WargamesGUI/Views/LoginPage.xaml.cs
+++ WargamesGUI/WargamesGUI/Views/LoginPage.xaml.cs
@@ -11,6 +11,8 @@ using WargamesGUI.Views;
 using Xamarin.Forms;
 using WargamesGUI.Services;
 using static WargamesGUI.AddUserPage;
+using Xamarin.Essentials;
+using WargamesGUI.ViewModels;
 
 namespace WargamesGUI
 {
@@ -19,13 +21,21 @@ namespace WargamesGUI
     {
         public static UserService service = new UserService();
         public static User user;
+        public static BookService2 bookService2 = new BookService2();
+
+        public List<Book2> bookList;
         public MainPage()
         {
             InitializeComponent();
-
+            MainThread.InvokeOnMainThreadAsync(async () => { await LoadBooks(); });
             //Entrypassword.Completed += (sender, e) => Entrypassword_Completed(sender, e);
         }
 
+        public async Task<List<Book2>> LoadBooks()
+        {
+            bookList = await bookService2.GetAllBooks();
+            return bookList;
+        }
         private async void SignIn_Button_Clicked(object sender, EventArgs e)
         {
             if (Entryusername.Text != "" & Entrypassword.Text != "")
@@ -90,6 +100,50 @@ namespace WargamesGUI
         private void Button_Clicked(object sender, EventArgs e)
         {
 
+        }
+
+        private void SearchBar_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            AutoCompleteList.IsVisible = true;
+            AutoCompleteList.BeginRefresh();
+
+            try
+            {
+                var titleResult = bookList.Select(x => x.Title).Where(x => x != null && x.ToUpper().Contains(SearchBar.Text.ToUpper())).ToList();
+                var publisherResult = bookList.Select(x => x.Publisher).Where(x => x != null && x.ToUpper().Contains(SearchBar.Text.ToUpper())).ToList();
+                var authorResult = bookList.Select(x => x.Author).Where(x => x != null && x.ToUpper().Contains(SearchBar.Text.ToUpper())).ToList();
+                var isbnResult = bookList.Select(x => x.ISBN).Where(x => x != null && x.ToUpper().Contains(SearchBar.Text.ToUpper())).ToList();
+                var subCategoryResult = bookList.Select(x => x.DeweySub.SubCategoryName).Where(x => x != null && x.ToUpper().Contains(SearchBar.Text.ToUpper())).ToList();
+                var mainCategoryResult = bookList.Select(x => x.DeweyMain.MainCategoryName).Where(x => x != null && x.ToString().ToUpper().Contains(SearchBar.Text.ToUpper())).ToList();
+
+                var allResults = titleResult.Concat(publisherResult).Concat(authorResult).Concat(isbnResult).Concat(subCategoryResult).Concat(mainCategoryResult).Distinct().ToList();
+
+                if (string.IsNullOrWhiteSpace(SearchBar.Text))
+                {
+                    AutoCompleteList.IsVisible = false;
+                }
+                else
+                {
+                    AutoCompleteList.ItemsSource = allResults;
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                //AutoCompleteList.IsVisible = false;
+            }
+
+          
+        }
+
+        private void AutoCompleteList_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            string listsd = e.Item as string;
+            SearchBar.Text = listsd;
+            AutoCompleteList.IsVisible = false;
+            ((ListView)sender).SelectedItem = null;
+            SearchBar_Clicked(sender, e);
         }
 
         //private async void Entrypassword_Completed(object sender, EventArgs e)
