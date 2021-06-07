f04944ac7cf2518717f0ddeb512ae75cc4dfb45e
ro-ed <robinedin94@gmail.com>
2021-06-07 13:51:56 +0200

    Update

--- WargamesGUI/WargamesGUI/App.xaml.cs
+++ WargamesGUI/WargamesGUI/App.xaml.cs
@@ -13,9 +13,9 @@ namespace WargamesGUI
         {         
             InitializeComponent();
             Device.SetFlags(new[] { "MediaElement_Experimental", "Brush_Experimental" });
-            MainPage = new MainPage();
+            //MainPage = new MainPage();
            // MainPage = new FlyoutLibrarianPage();
-            //MainPage = new FlyoutAdminPage();
+            MainPage = new FlyoutAdminPage();
             //MainPage = new VisitorPage();
             //MainPage = new AddUserPage();
             //MainPage = new Test();
